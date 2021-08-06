7a8803042ed65aa665f995f6d1cbe6620912d7bc
Roberto Camargo <jcamargoriv@gmail.com>
2021-08-06 16:21:41 -0500

    Comienzo del curso

---
+++ bin/dart_application_udemy.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_udemy/dart_application_udemy.dart' as dart_application_udemy;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_udemy.calculate()}!');
+}
---
+++ lib/dart_application_udemy.dart
@@ -0,0 +1,8 @@
+int calculate() {
+  int a = 20;
+  int b = 20;
+
+  a = 30;
+  b = 40;
+  return a * b;
+}
